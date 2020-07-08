0e9be690b0cca5f5449f7e42fe423aff17d12d29
Aidan Ginise <giniseaidan@gmail.com>
2020-07-08 15:12:32 -0700

    new function in youtube: - returnId will take a youtube video url and return the id

--- core/youtube.py
+++ core/youtube.py
@@ -1,28 +1,23 @@
 import urllib.request
 import json
 import urllib
-import pprint
 
 def returnId(url):
 
     #making sure this is a youtube video
-    if 'https://www.youtube.com/watch?v' in url:
-
+    if 'https://www.youtube.com/watch?v=' in url:
         idString = url.split('watch?v=')[1]
 
         if '&feature=' in idString:
             return idString.split('&feature=')[0]
-
         else:
             return idString
-
     #isnt a youtube video
-    if 'https://www.youtube.com/watch?v' not in url:
+    if 'https://www.youtube.com/watch?v=' not in url:
         return False
 
 #getting data from the youtube video
 def getVidTitle(id):
-
     # defining the things to send
     params = {"format": "json", "url": f"https://www.youtube.com/watch?v={id}"}
     url = "https://www.youtube.com/oembed"
