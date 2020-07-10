55dc90092d8accbf472b944006c053d9f5e25fad
Aidan Ginise <giniseaidan@gmail.com>
2020-07-09 18:57:20 -0700

    minor fixes

--- core/reddit.py
+++ core/reddit.py
@@ -31,30 +31,30 @@ class RedditClass:
         self.confidences = [tf.confidenceCheck(t) for t in self.titles]
 
         # this is cancer
-        self.head + =  f"This comment contains {ul} youtube videos which are likely to be rickrolls!" if self.ul > 1 else \
-        "This comment contains a youtube video that is a rickroll!" if self.confidences[0] > =  85 else \
-        "I have detected a YouTube video that appears to be a rickroll in this comment!" if 60 < =  self.confidences[0] < 70 else \
-        "The link in this comment is a YouTube video that may be a rickroll!" if 60 < =  self.confidences[0] < 70 else None
+        self.head +=  f"This comment contains {ul} youtube videos which are likely to be rickrolls!" if self.ul > 1 else \
+        "This comment contains a youtube video that is a rickroll!" if self.confidences[0] >=  85 else \
+        "I have detected a YouTube video that appears to be a rickroll in this comment!" if 60 <=  self.confidences[0] < 70 else \
+        "The link in this comment is a YouTube video that may be a rickroll!" if 60 <=  self.confidences[0] < 70 else None
 
         # initializing our body
         self.body = ""
 
         # if only one url
-        if self.ul = =  1:
-            self.body + =  f" I am {self.confidences[0]}% confident of this.\n\n"\
+        if self.ul ==  1:
+            self.body +=  f" I am {self.confidences[0]}% confident of this.\n\n"\
             f"The link's destination is [{self.unUrls[0]}]({self.unUrls[0]}), a YouTube video with a title of: \"{self.titles[0]}\".\n\n"
         # this is also cancer
         else:
-            self.body + =  "\n\nThese are as follows:\n\n"
+            self.body +=  "\n\nThese are as follows:\n\n"
 
-            self.body + =  ''.join([f"[{self.unUrls[i]}]({self.unUrls[i]}) - Video with title {self.titles[i]}. I am {self.confidences[i]}%"\
+            self.body +=  ''.join([f"[{self.unUrls[i]}]({self.unUrls[i]}) - Video with title {self.titles[i]}. I am {self.confidences[i]}%"\
             f" confident this is a rickroll.\n\n" for i in enumerate(self.urls)])
 
         # adding final shit
-        self.body + =  "^(I am a Reddit bot with the sole purpose of preventing rickrolls. For me information on me and how I work, visit) ^r/RickMeNot. ^(Please consider donating to my creator) ^[here.](https://www.paypal.me/aidanginise1)"
+        self.body +=  "^(I am a Reddit bot with the sole purpose of preventing rickrolls. For me information on me and how I work, visit) ^r/RickMeNot. ^(Please consider donating to my creator) ^[here.](https://www.paypal.me/aidanginise1)"
 
         # and finally, return it
-        return f"{self.head}\n\n{self.body}"
+        return f"{self.head}{self.body}"
 
     def commentStream(self):
         self.subreddit = self.reddit.subreddit('mytestsubgoaway')
@@ -68,15 +68,15 @@ class RedditClass:
                     self.err = "[STREAM] "
 
                     if isinstance(e, APIException):
-                        self.err + =  "API error occurred:"
+                        self.err +=  "API error occurred:"
                     elif isinstance(e, ClientException):
-                        self.err + =  "Client-side error occurred:"
+                        self.err +=  "Client-side error occurred:"
                     elif isinstance(e, Forbidden):
-                        self.err + =  f"Bot is banned from subreddit {comment.subreddit}:"
+                        self.err +=  f"Bot is banned from subreddit {comment.subreddit}:"
                     else:
-                        self.err + =  "Unknown error occurred:"
+                        self.err +=  "Unknown error occurred:"
 
-                    self.err + =  f"{e}"
+                    self.err +=  f"{e}"
 
                     print(self.err)
 
