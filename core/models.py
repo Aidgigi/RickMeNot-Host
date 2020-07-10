fb7bd73345f478d5b2eb90bbd348900c6970fdc2
Aidan Ginise <giniseaidan@gmail.com>
2020-07-09 22:11:18 -0700

    new model for logging rolls, and a modified stats table

--- core/models.py
+++ core/models.py
@@ -1,29 +1,30 @@
 #importing all of the things we need for the database models
-from sqlalchemy import Column, Integer, BigInteger, String
+from sqlalchemy import Column, Integer, BigInteger, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 # this table is for keeping track of statistics
-class StatsTable(Base):
-    __tablename__ = "BOT_STATISTICS"
+class Stats(Base):
+    __tablename__ = "BOT_STATISTICS_TABLE"
 
-    id = Column(Integer, primary_key=True)
-    total_roll_count = Column(BigInteger)
-    biggest_roller = Column(String)
-    redirects_used = Column(String)
-    urls_used = Column(String)
+    id = Column(BigInteger, primary_key = True)
+    roll_count = Column(String)
+    roller_board = Column(String)
+    subreddit_board = Column(String)
+    video_board = Column(String)
 
+class RollLog(Base):
+    __tablename__ = "ROLL_LOG_TABLE"
 
-    def repr(self):
-        return 'Bot Statistics Table'
-
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    data = Column(String)
 
 # this table defines subreddits that are on the blacklist
 class Blacklist(Base):
-    __tablename__ = "SUBREDDIT_BLACKLIST"
+    __tablename__ = "SUBREDDIT_BLACKLIST_TABLE"
 
-    id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, primary_key = True)
     subreddit = Column(String)
     opt_reason = Column(String)
